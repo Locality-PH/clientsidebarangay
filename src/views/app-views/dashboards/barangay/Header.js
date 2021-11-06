702edb5bc7681dcc719314630a1cc26b8633c687
RojhonBB <buenaventurarojhon5@gmail.com>
2021-11-06 08:19:46 +0800

    Barangay Header

--- src/views/app-views/dashboards/barangay/Header.js
+++ src/views/app-views/dashboards/barangay/Header.js
@@ -7,92 +7,82 @@ import {
 	HomeOutlined,
 	PhoneOutlined
 } from '@ant-design/icons';
-import PageHeaderAlt from 'components/layout-components/PageHeaderAlt'
 import Flex from 'components/shared-components/Flex'
 
 const Header = ({name, avatarSize, quote, email, phoneNumber, address, website}) => {
 	return (
 			<>
-				<PageHeaderAlt background="/img/others/img-12.jpg" cssClass="bg-primary" overlap>
-					<div className="container text-center">
-						<div className="py-5 my-md-5">
-						</div>
-					</div>
-				</PageHeaderAlt>
-
-				<div className="container my-4">
-					<Card>
-						<Row justify="center"> 
-							<Col sm={24} md={23}>
-								<div className="d-md-flex">
-									{/* <div className="rounded p-2 bg-white shadow-sm mx-auto" style={{'marginTop': '-3.5rem', 'maxWidth': `${props.avatarSize + 16}px`}}>
-										<Avatar shape="square" size={props.avatarSize} src="/img/barangay/caniogan/BarangayProfile.jpg" />
-									</div> */}
-									<div style={{'marginTop': '-3.5rem', 'maxWidth': `${avatarSize + 16}px`}}>
-										<Avatar shape="round" size={avatarSize} src="/img/barangay/caniogan/BarangayProfile.jpg" />
-									</div>
-									<div className="ml-md-4 w-100">
-										<Flex alignItems="center" mobileFlex={false} className="mb-3 text-md-left text-center">
-											<h2 className="mb-0 mt-md-0 mt-2">Barangay {name}</h2>
-											<div className="ml-md-3 mt-3 mt-md-0">
-												<Button size="small" type="primary">Recident</Button>
-												<Button size="small" type="primary" className="ml-2">Blotter</Button>
-												<Button size="small" className="ml-2">Message</Button>
-											</div>
-										</Flex>
-										<Row gutter="16"> 
-											<Col sm={24} md={8}>
-												<p className="mt-0 mr-3 text-muted text-md-left text-center">{quote}</p>
-											</Col>
-											<Col xs={24} sm={24} md={8}>
-												<Row className="mb-2"> 
-													<Col xs={12} sm={12} md={9}>
-														<Icon type={MailOutlined} className="text-primary font-size-md"/>
-														<span className="text-muted ml-2">Email:</span>
-													</Col>
-													<Col xs={12} sm={12} md={15}>
-														<span className="font-weight-semibold">{email}</span>
-													</Col>
-												</Row>
-												<Row> 
-													<Col xs={12} sm={12} md={9}>
-														<Icon type={PhoneOutlined} className="text-primary font-size-md"/>
-														<span className="text-muted ml-2">Phone:</span>
-													</Col>
-													<Col xs={12} sm={12} md={15}>
-														<span className="font-weight-semibold">{phoneNumber}</span>
-													</Col>
-												</Row>
-											</Col>
-											<Col xs={24} sm={24} md={8}>
-												<Row className="mb-2 mt-2 mt-md-0 "> 
-													<Col xs={12} sm={12} md={9}>
-														<Icon type={HomeOutlined} className="text-primary font-size-md"/>
-														<span className="text-muted ml-2">Address:</span>
-													</Col>
-													<Col xs={12} sm={12} md={15}>
-														<span className="font-weight-semibold">{address}</span>
-													</Col>
-												</Row>
-												<Row className="mb-2"> 
-													<Col xs={12} sm={12} md={9}>
-														<Icon type={GlobalOutlined} className="text-primary font-size-md"/>
-														<span className="text-muted ml-2">Website:</span>
-													</Col>
-													<Col xs={12} sm={12} md={15}>
-														<span className="font-weight-semibold">{website}</span>
-													</Col>
-												</Row>
-											</Col>
-										</Row>
-									</div>
+				<Card>
+					<Row justify="center"> 
+						<Col sm={24} md={23}>
+							<div className="d-md-flex">
+								{/* <div className="rounded p-2 bg-white shadow-sm mx-auto" style={{'marginTop': '-3.5rem', 'maxWidth': `${props.avatarSize + 16}px`}}>
+									<Avatar shape="square" size={props.avatarSize} src="/img/barangay/caniogan/BarangayProfile.jpg" />
+								</div> */}
+								<div style={{'marginTop': '-3.5rem', 'maxWidth': `${avatarSize + 16}px`}}>
+									<Avatar shape="round" size={avatarSize} src="/img/barangay/caniogan/BarangayProfile.jpg" />
 								</div>
-							</Col>
-						</Row>
-					</Card>
-				</div>
+								<div className="ml-md-4 w-100">
+									<Flex alignItems="center" mobileFlex={false} className="mb-3 text-md-left text-center">
+										<h2 className="mb-0 mt-md-0 mt-2">Barangay {name}</h2>
+										<div className="ml-md-3 mt-3 mt-md-0">
+											<Button size="small" type="primary">Recident</Button>
+											<Button size="small" type="primary" className="ml-2">Blotter</Button>
+											<Button size="small" className="ml-2">Message</Button>
+										</div>
+									</Flex>
+									<Row gutter="16"> 
+										<Col sm={24} md={8}>
+											<p className="mt-0 mr-3 text-muted text-md-left text-center">{quote}</p>
+										</Col>
+										<Col xs={24} sm={24} md={8}>
+											<Row className="mb-2"> 
+												<Col xs={12} sm={12} md={9}>
+													<Icon type={MailOutlined} className="text-primary font-size-md"/>
+													<span className="text-muted ml-2">Email:</span>
+												</Col>
+												<Col xs={12} sm={12} md={15}>
+													<span className="font-weight-semibold">{email}</span>
+												</Col>
+											</Row>
+											<Row> 
+												<Col xs={12} sm={12} md={9}>
+													<Icon type={PhoneOutlined} className="text-primary font-size-md"/>
+													<span className="text-muted ml-2">Phone:</span>
+												</Col>
+												<Col xs={12} sm={12} md={15}>
+													<span className="font-weight-semibold">{phoneNumber}</span>
+												</Col>
+											</Row>
+										</Col>
+										<Col xs={24} sm={24} md={8}>
+											<Row className="mb-2 mt-2 mt-md-0 "> 
+												<Col xs={12} sm={12} md={9}>
+													<Icon type={HomeOutlined} className="text-primary font-size-md"/>
+													<span className="text-muted ml-2">Address:</span>
+												</Col>
+												<Col xs={12} sm={12} md={15}>
+													<span className="font-weight-semibold">{address}</span>
+												</Col>
+											</Row>
+											<Row className="mb-2"> 
+												<Col xs={12} sm={12} md={9}>
+													<Icon type={GlobalOutlined} className="text-primary font-size-md"/>
+													<span className="text-muted ml-2">Website:</span>
+												</Col>
+												<Col xs={12} sm={12} md={15}>
+													<span className="font-weight-semibold">{website}</span>
+												</Col>
+											</Row>
+										</Col>
+									</Row>
+								</div>
+							</div>
+						</Col>
+					</Row>
+				</Card>
 			</>
 		)
-	}
+}
 
 export default Header;
--- src/views/app-views/dashboards/barangay/index.js
+++ src/views/app-views/dashboards/barangay/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Row, Col, Card, Typography, Button} from 'antd';
-import BarangayLinks from "../default/BarangayLinks";
+import HeaderCover from "./HeaderCover";
 import Header from "./Header";
 import Barangay from "./Barangay";
 import { 
@@ -14,11 +14,15 @@ const Index = ({match}) => {
 
   return (
     <> 
+      <HeaderCover img="/img/others/img-12.jpg"></HeaderCover>
+
+      <div className="container my-4">
         <Header name={match.params.name} avatarSize={150}
-        quote="We rise and fall as one people, one nation."
-        email="caniogan@gmail.com" phoneNumber="+12 123 1234" address="Caniogan Morong, Rizal"
-        website="facebook.com"></Header>
-        {/* <Barangay name={match.params.name}></Barangay> */}
+          quote="We rise and fall as one people, one nation."
+          email="caniogan@gmail.com" phoneNumber="+12 123 1234" address="Caniogan Morong, Rizal"
+          website="facebook.com"></Header>
+      </div>
+      {/* <Barangay name={match.params.name}></Barangay> */}
     </>
   )
 }
