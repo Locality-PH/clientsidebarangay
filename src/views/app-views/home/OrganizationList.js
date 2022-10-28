c55b445779250cf46940cf76beaf03979bd0a0e3
Rojhon <rojhonbuenaventura5@gmail.com>
2022-10-28 14:53:28 -0700

    Organization List

--- src/views/app-views/home/OrganizationList.js
+++ src/views/app-views/home/OrganizationList.js
@@ -6,59 +6,59 @@ import axios from "axios";
 import { useAuth } from "contexts/AuthContext";
 
 const OrganizationList = () => {
-  const { currentOrganization, generateToken } = useAuth();
+    const { currentOrganization, generateToken } = useAuth();
 
-  const [organizationList, setOrganizationList] = useState(BarangayData);
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    getAllOrganizations();
-  }, []);
+    const [organizationList, setOrganizationList] = useState(BarangayData);
+    const [isLoading, setIsLoading] = useState(true);
+    
+    useEffect(() => {
+        getAllOrganizations();
+    }, []);
 
-  const getAllOrganizations = async () => {
-    setOrganizationList(BarangayData);
-    setIsLoading(false);
+    const getAllOrganizations = async () => {
+        await axios
+            .get("/api/organization/get-latest-organizations", generateToken()[1])
+            .then((response) => {
+                setOrganizationList(response.data);
+                setIsLoading(false);
 
-    await axios
-      .get("/api/organization/get-all-organizations", generateToken()[1])
-      .then((response) => {
-        console.log("Organizations ", response.data);
-      })
-      .catch((err) => {
-        message.error("Could not fetch the data in the server!");
-        console.log(err);
-      });
-  };
+            })
+            .catch((err) => {
+                message.error("Could not fetch the data in the server!");
+                console.log(err);
+            });
+    };
 
-  return (
-    <>
-      <Card
-        title="List of Barangay"
-        loading={isLoading}
-        extra={
-          <a href="#" style={{ fontSize: "1rem" }}>
-            More
-          </a>
-        }
-      >
-        <div className="mt-3">
-          {organizationList.map((result, i) => (
-            <div
-              key={i}
-              className={`d-flex align-items-center justify-content-between mb-4`}
+    return (
+        <>
+            <Card
+                title="List of Barangay"
+                loading={isLoading}
+                extra={
+                    <a href="#" style={{ fontSize: "1rem" }}>
+                        More
+                    </a>
+                }
             >
-              <GroupLink
-                key={i}
-                id={i}
-                src={result.img}
-                name={result.name}
-                subTitle={result.title}
-              />
-            </div>
-          ))}
-        </div>
-      </Card>
-    </>
-  );
+                <div className="mt-3">
+                    {organizationList.map((result, i) => (
+                        <div
+                            key={i}
+                            className={`d-flex align-items-center justify-content-between mb-4`}
+                        >
+                            <GroupLink
+                                key={i}
+                                id={i}
+                                // src={result.img}
+                                name={result.organization_name}
+                                subTitle={result.address}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </Card>
+        </>
+    );
 };
 
 export default OrganizationList;
