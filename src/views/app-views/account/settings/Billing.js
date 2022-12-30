83ac24d04b529ca1602d606cf09a7f5db989bf3a
vonypeto <von.aralar@gmail.com>
2022-12-30 08:23:32 +0800

    setting: added billing and arrow function

--- src/views/app-views/account/settings/Billing.js
+++ src/views/app-views/account/settings/Billing.js
@@ -1,246 +1,249 @@
-import React, { Component } from 'react'
-import { Table, Button, Tooltip, Form, Modal, Input, Row, Col, Card } from 'antd';
-import { DeleteOutlined, CreditCardOutlined, CalendarOutlined, QuestionCircleOutlined } from '@ant-design/icons';
-import { ROW_GUTTER } from 'constants/ThemeConstant';
+import React, { useState, useEffect } from "react";
+import {
+  Table,
+  Button,
+  Tooltip,
+  Form,
+  Drawer,
+  Modal,
+  Input,
+  Row,
+  Col,
+  Card,
+} from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
+import BillingDrawer from "components/shared-components/Drawer";
+import { AUTH_TOKEN } from "redux/constants/Auth";
 
-const { Column } = Table;
+const cc_format = (value) => {
+  var v = value;
 
-const AddNewCardForm = ({ visible, onCreate, onCancel }) => {
-  const [form] = Form.useForm();
-  return (
-    <Modal
-      title="Add new card"
-      visible={visible}
-      okText="Save card"
-      onCancel={onCancel}
-      onOk={() => {
-        form
-          .validateFields()
-          .then(values => {
-            form.resetFields();
-            onCreate(values);
-          })
-          .catch(info => {
-            console.log('Validate Failed:', info);
-          });
-      }}
-    >
-      <Form
-        form={form}
-        name="addCardForm"
-        layout="vertical"
-      >
-        <Form.Item
-          label="Card holder name"
-          name="cardHolderName"
-          rules={
-            [
-              { 
-                require: true,
-                message: 'Please enter card holder name!' 
-              }
-            ]
-          }
-        >
-          <Input suffix={<CreditCardOutlined />} placeholder="Card holder name" />
-        </Form.Item>
-        <Form.Item
-          label="Card number"
-          name="cardNumber"
-          hasFeedback
-          rules={
-            [
-              { 
-                pattern: /(\d{4}[-. ]?){4}|\d{4}[-. ]?\d{6}[-. ]?\d{5}/g,
-                message: 'Please enter a valid credit card number!' 
-              }
-            ]
-          }
-        >
-          <Input suffix={<CreditCardOutlined />} placeholder="0000 0000 0000 00" />
-        </Form.Item>
-        <Row gutter={ROW_GUTTER}>
-          <Col xs={24} sm={24} md={12}>
-            <Form.Item
-              label="Expiry date"
-              name="exp"
-              rules={
-                [
-                  { 
-                    pattern: /^(0[1-9]|1[0-2])[- /.]\d{2}/,
-                    message: 'Please enter a valid date format!' 
-                  }
-                ]
-              }
-            >
-              <Input suffix={<CalendarOutlined />} placeholder="MM/YY" />
-            </Form.Item>
-          </Col>
-          <Col xs={24} sm={24} md={12}>
-            <Form.Item
-              label="CVV code"
-              name="cvv"
-              rules={
-                [
-                  { 
-                    pattern: /^[0-9]{3,4}$/,
-                    message: 'Please enter a CVV code format!' 
-                  }
-                ]
-              }
-            >
-              <Input 
-                suffix={
-                  <Tooltip title="The last three digits printed on the back of the card">
-                    <QuestionCircleOutlined className="cursor-pointer" />
-                  </Tooltip>
-                } 
-                placeholder="000"
-                />
-            </Form.Item>
-          </Col>
-        </Row>
-      </Form>
-    </Modal>
-  )
-}
+  var match = v;
+  var parts = [];
 
-export class Billing extends Component {
-
-	state = {
-    selectedRowKeys: ["card-1"], // Check here to configure the default column
-    creditCards : [
-      {
-        key: 'card-1',
-        cardType: 'Visa',
-        cardTypeImg: '/img/others/img-8.png',
-        cardNumber: '•••• •••• •••• 7260',
-        exp: '06/22'
-      },
-      {
-        key: 'card-2',
-        cardType: 'Master',
-        cardTypeImg: '/img/others/img-9.png',
-        cardNumber: '•••• •••• •••• 1272',
-        exp: '04/21'
-      }
-    ],
-    modalVisible: false,
-    newCreditCardInfo: {
-      cardHolderName: '',
-      cardNumber: '',
-      exp: '06/22'
-    }
-  };
-
-  onSelectChange = (selectedRowKeys, selectedRows) => {
-    this.setState({ selectedRowKeys });
-  };
-
-  showModal = () => {
-    this.setState({
-      modalVisible: true,
-    });
-  };
-
-  closeModal = () => {
-    this.setState({
-      modalVisible: false,
-    });
+  for (var i = 1, len = match.length; i < len; i += 4) {
+    parts.push(match.substring(i, i + 4));
   }
 
-  addCard = values => {
-    const { cardNumber, exp } = values
-    const cardType = [
-      {
-        img: '/img/others/img-8.png',
-        type: 'Visa'
-      },
-      {
-        img: '/img/others/img-9.png',
-        type: 'Master'
-      }
-    ]
-    const randomCardType = cardType[Math.floor(Math.random() * cardType.length)];
-    const reg = /\b(?:\d{4}[ -]?){3}(?=\d{4}\b)/gm
-    const substr = `•••• •••• •••• `
-    const cardInfo = {
-      key: `card-${this.state.creditCards.length + 1}`,
-      cardType: randomCardType.type,
-      cardTypeImg: randomCardType.img,
-      cardNumber: cardNumber.replace(reg, substr),
-      exp: exp
-    }
-    this.setState({
-      modalVisible: false,
-      creditCards: [
-        ...this.state.creditCards,
-        cardInfo
-      ]
-    })
+  if (parts.length) {
+    return parts.join(" ");
+  } else {
+    return value;
+  }
+};
+const tableColumns = [
+  {
+    title: "Card type",
+    dataIndex: "issuer",
+    key: "issuer",
+    render: (_, record) => {
+      let image = "";
+      if (record.issuer == "mastercard") image = "/img/others/img-9.png";
+
+      if (record.issuer == "visa") image = "/img/others/img-8.png";
+
+      if (record.issuer == "GCash") image = "/img/others/cards/GCash-Logo.png";
+      return (
+        <>
+          {record.issuer == "GCash" ? (
+            <img style={{ height: "13%" }} src={image} alt={record.cardType} />
+          ) : (
+            <img style={{ height: "auto" }} src={image} alt={record.cardType} />
+          )}
+
+          <span className="ml-2">{record.issuer}</span>
+        </>
+      );
+    },
+  },
+
+  {
+    title: "Card Number",
+    dataIndex: "cardNumber",
+    key: "cardNumber",
+    render: (_, record) => {
+      let creditFormat;
+      if (record.cardNumber) creditFormat = cc_format(record.cardNumber);
+      return (
+        <>
+          <span className="ml-2">
+            {cc_format(
+              record.cardNumber.replace(
+                record.cardNumber.substr(0, record.cardNumber.length - 4),
+                record.cardNumber
+                  .substr(1, record.cardNumber.length - 3)
+                  .replace(/./g, "•")
+              )
+            )}
+          </span>
+        </>
+      );
+    },
+  },
+
+  {
+    title: "Expiry",
+    dataIndex: "validThru",
+    key: "validThru",
+  },
+  {
+    title: () => <div className="text-right"></div>,
+    key: "actions",
+    render: (_, record) => (
+      <Tooltip title="Remove card">
+        <Button
+          type="text"
+          shape="circle"
+          icon={<DeleteOutlined />}
+          onClick={() => {}}
+        />
+      </Tooltip>
+    ),
+  },
+];
+
+const Billing = () => {
+  const tmp = [
+    {
+      cardHolder: "12321",
+      cardNumber: "1232132113112323",
+      card_id: 1,
+      cvc: "N/A",
+      issuer: "visa",
+      user_id: "kwWXpExbfoTAVnQZQVeVqMzCRFP2",
+      validThru: "N/A",
+    },
+  ];
+  const [creditCards, setCreditCards] = useState(tmp);
+  const [paymentMethod, setPaymentMethod] = useState({});
+  const [paymentMethodType, setPaymentMethodType] = useState("");
+  const [selectedRowKeys, setSelectedRowKeys] = useState("card-1");
+  console.log(creditCards);
+  const [drawer, setDrawer] = useState(false);
+  const locale = {
+    emptyText: (
+      <div className="my-4 text-center">
+        <img
+          src="/img/others/img-7.png"
+          alt="Add credit card"
+          style={{ maxWidth: "90px" }}
+        />
+        <h3 className="mt-3 font-weight-light">Please add a payment method!</h3>
+      </div>
+    ),
+  };
+  console.log(paymentMethod);
+  const showDrawer = (type) => {
+    console.log("open");
+    setPaymentMethodType(type);
+    setDrawer(true);
   };
 
-	render() {
-		const { selectedRowKeys, creditCards, modalVisible } = this.state;
-    const rowSelection = {
-      selectedRowKeys,
-      type: 'radio',
-      onChange: this.onSelectChange,
-    };
+  const closeDrawer = () => {
+    setDrawer(false);
+  };
 
-    const locale = {
-      emptyText: (
-        <div className="text-center my-4">
-          <img src="/img/others/img-7.png" alt="Add credit card" style={{maxWidth: '90px'}}/>
-          <h3 className="mt-3 font-weight-light">Please add a credit card!</h3>
+  useEffect(() => {
+    if (!(JSON.stringify(paymentMethod) === "{}")) {
+      let credit = creditCards;
+      let payment = paymentMethod;
+      payment.user_id = localStorage.getItem(AUTH_TOKEN);
+      payment.card_id = credit.length + 1;
+      setCreditCards((oldArray) => [...oldArray, payment]);
+    }
+  }, [paymentMethod]);
+  return (
+    <>
+      <BillingDrawer
+        forceRender
+        visible={drawer}
+        paymentMethod={paymentMethod}
+        setPaymentMethod={setPaymentMethod}
+        setDrawer={setDrawer}
+        paymentType={paymentMethodType}
+        close={() => {
+          closeDrawer();
+        }}
+      />
+
+      <Card className="setting-content">
+        <h2 className="mb-2">Billing</h2>
+        <Table
+          locale={locale}
+          dataSource={creditCards}
+          // rowSelection={rowSelection}
+          columns={tableColumns}
+          pagination={false}
+          rowKey="card_id"
+        />
+        <div className="mt-3 text-left">
+          <Row gutter={24}>
+            <Col sm={24} md={24} lg={24} className="text-left">
+              <h4 className="mb-2">Credit Card/Debit Card</h4>
+            </Col>
+            <Col sm={24} md={24} lg={14}>
+              <Button
+                style={{ padding: "0 20px" }}
+                className="card-platform"
+                onClick={() => showDrawer("credit")}
+              >
+                <Row justify="space-between" className=" d-flex">
+                  <Col className="text-left">
+                    <img
+                      style={{ width: "9%", marginRight: "5px" }}
+                      src="/img/others/img-7.png"
+                    />
+                    Credit Card/Debit Card
+                  </Col>
+                  <Col sm={5} md={5} lg={5}>
+                    <img
+                      className="card-brand"
+                      style={{ width: "25%" }}
+                      src="/img/others/cards/amex.png"
+                    />
+                    <img
+                      style={{ width: "25%" }}
+                      className="card-brand"
+                      src="/img/others/cards/jcb.png"
+                    />
+                    <img
+                      style={{ width: "25%" }}
+                      className="card-brand"
+                      src="/img/others/cards/mc.png"
+                    />
+                    <img
+                      style={{ width: "25%" }}
+                      className="card-brand"
+                      src="/img/others/cards/visa.png"
+                    />
+                  </Col>
+                </Row>
+              </Button>
+            </Col>
+            <Col sm={24} md={24} lg={24} className="text-left">
+              <h4 className="mt-2">E-Wallet</h4>
+            </Col>
+            <Col sm={24} md={24} lg={12}>
+              <Button
+                style={{ padding: "0 20px" }}
+                className="card-platform"
+                onClick={() => showDrawer("e-wallet")}
+              >
+                <Row justify="space-between" className=" d-flex">
+                  <Col className="text-left">
+                    <Row>
+                      <h4 className="mt-2 mr-1">GCash</h4>
+                      <span className="pt-2-1">GCash</span>
+                    </Row>
+                  </Col>
+                </Row>
+              </Button>
+            </Col>
+          </Row>
         </div>
-      )
-    };
+      </Card>
+    </>
+  );
+};
 
-		return (
-			<Card className="setting-content">
-				<h2 className="mb-4">Billing</h2>
-				<Table locale={locale} dataSource={creditCards} rowSelection={rowSelection} pagination={false}>
-					<Column 
-            title="Card type" 
-            key="cardType" 
-            render={(text, record) => (
-              <>
-                <img src={record.cardTypeImg} alt={record.cardType} />
-                <span className="ml-2">{record.cardType}</span>
-              </>
-            )}
-          />
-					<Column title="Card Number" dataIndex="cardNumber" key="cardNumber" />
-					<Column title="Expires on" dataIndex="exp" key="exp" />
-          <Column 
-            title="" 
-            key="actions"
-            className="text-right" 
-            render={(text, record) => (
-              <Tooltip title="Remove card">
-                <Button 
-                  type="text" 
-                  shape="circle" 
-                  icon={<DeleteOutlined />} 
-                  onClick={() => {
-                    const newCreditCards = [...creditCards];
-                    this.setState({
-                      creditCards: newCreditCards.filter(item => item.key !== record.key),
-                    });
-                  }} 
-                />
-              </Tooltip>
-            )}
-          />
-				</Table>
-        <div className="mt-3 text-right">
-          <Button type="primary" onClick={this.showModal}>Add new card</Button>
-        </div>
-          <AddNewCardForm visible={modalVisible} onCreate={this.addCard} onCancel={this.closeModal}/>
-			</Card>
-		)
-	}
-}
-
-export default Billing
+export default Billing;
--- src/views/app-views/account/settings/EditProfile.js
+++ src/views/app-views/account/settings/EditProfile.js
@@ -1,178 +1,156 @@
-import React, { Component } from 'react';
-import { Form, Avatar, Button, Input, DatePicker, Row, Col, message, Upload, Card } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
-import { ROW_GUTTER } from 'constants/ThemeConstant';
-import Flex from 'components/shared-components/Flex'
+import React, { useState, useEffect } from "react";
+import {
+  Form,
+  Avatar,
+  Button,
+  Input,
+  DatePicker,
+  Row,
+  Col,
+  message,
+  Upload,
+  Card,
+} from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import { ROW_GUTTER } from "constants/ThemeConstant";
+import Flex from "components/shared-components/Flex";
+const EditPorfile = () => {
+  const [formData, setFormData] = useState({});
+  const [form] = Form.useForm();
 
-export class EditProfile extends Component {
+  useEffect(() => {
+    setFormData({
+      avatarUrl: "/img/avatars/thumb-6.jpg",
+      name: "Charlie Howard",
+      email: "charlie.howard@themenate.com",
+      userName: "Charlie",
+      dateOfBirth: null,
+      phoneNumber: "+44 (1532) 135 7921",
+      address1: "San Isidro St",
+      city: "Morong",
+      country: "Philippines",
+      postcode: "1972",
+    });
+    form.setFieldsValue({
+      avatarUrl: "/img/avatars/thumb-6.jpg",
+      name: "Charlie Howard",
+      email: "charlie.howard@themenate.com",
+      userName: "Charlie",
+      dateOfBirth: null,
+      phoneNumber: "+44 (1532) 135 7921",
+      website: "",
+      address1: "San Isidro St",
+      city: "Morong",
+      country: "Philippines",
 
-	avatarEndpoint = 'https://www.mocky.io/v2/5cc8019d300000980a055e76'
+      postcode: "1972",
+    });
+  }, []);
+  return (
+    <>
+      <Form
+        name="basicInformation"
+        layout="vertical"
+        form={form}
+        initialValues={formData}
+      >
+        <Card className="setting-content">
+          <Flex
+            alignItems="center"
+            mobileFlex={false}
+            className="text-center text-md-left"
+          >
+            <Avatar
+              size={90}
+              src={formData?.avatarUrl}
+              icon={<UserOutlined />}
+            />
+            <div className="mt-3 ml-3 mt-md-0">
+              <Button type="primary">Change Avatar</Button>
 
-	state = {
-		avatarUrl: '/img/avatars/thumb-6.jpg',
-		name: 'Charlie Howard',
-		email: 'charlie.howard@themenate.com',
-		userName: 'Charlie',
-		dateOfBirth: null,
-		phoneNumber: '+44 (1532) 135 7921',
-		website: '',
-		address: 'San Isidro St',
-		city: 'Morong',
-		postcode: '1972'
-	}
+              <Button className="ml-2">Remove</Button>
+            </div>
+          </Flex>
+          <div className="mt-4">
+            <Row>
+              <Col xs={24} sm={24} md={24} lg={24}>
+                <Row gutter={ROW_GUTTER}>
+                  <Col xs={24} sm={24} md={12}>
+                    <Form.Item
+                      label="Name"
+                      name="name"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your name!",
+                        },
+                      ]}
+                    >
+                      <Input />
+                    </Form.Item>
+                  </Col>
 
+                  <Col xs={24} sm={24} md={12}>
+                    <Form.Item
+                      label="Email"
+                      name="email"
+                      rules={[
+                        {
+                          required: true,
+                          type: "email",
+                          message: "Please enter a valid email!",
+                        },
+                      ]}
+                    >
+                      <Input disabled />
+                    </Form.Item>
+                  </Col>
+                </Row>
+                <Button type="primary" htmlType="submit">
+                  Save Change
+                </Button>
+              </Col>
+            </Row>
+          </div>
+        </Card>
+        <Card className="setting-content" title={"Billing Information"}>
+          <Row gutter={ROW_GUTTER}>
+            {" "}
+            <Col xs={24} sm={24} md={24}>
+              <Form.Item label="Address 1" name="address1">
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={24}>
+              <Form.Item label="Address 2" name="address2">
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item label="Phone Number" name="phoneNumber">
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item label="City" name="city">
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item label="Country" name="country">
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item label="Post code" name="postcode">
+                <Input />
+              </Form.Item>
+            </Col>
+          </Row>
+        </Card>
+      </Form>
+    </>
+  );
+};
 
-	render() {
-		const onFinish = values => {
-			const key = 'updatable';
-			message.loading({ content: 'Updating...', key });
-			setTimeout(() => {
-				this.setState({
-					name: values.name,
-					email: values.email,
-					userName: values.userName,
-					dateOfBirth: values.dateOfBirth,
-					phoneNumber: values.phoneNumber,
-					address: values.address,
-					city: values.city,
-					postcode: values.postcode,
-				})
-				message.success({ content: 'Done!', key, duration: 2 });
-			}, 1000);
-		};
-
-		const { name, email, userName, dateOfBirth, phoneNumber, website, address, city, postcode, avatarUrl } = this.state;
-
-		return (
-			<Card className="setting-content">
-				<Flex alignItems="center" mobileFlex={false} className="text-center text-md-left">
-					<Avatar size={90} src={avatarUrl} icon={<UserOutlined />} />
-					<div className="ml-3 mt-md-0 mt-3">
-						<Button type="primary">Change Avatar</Button>
-
-						<Button className="ml-2">Remove</Button>
-					</div>
-				</Flex>
-				<div className="mt-4">
-					<Form
-						name="basicInformation"
-						layout="vertical"
-						initialValues={
-							{
-								'name': name,
-								'email': email,
-								'username': userName,
-								'dateOfBirth': dateOfBirth,
-								'phoneNumber': phoneNumber,
-								'address': address,
-								'city': city,
-								'postcode': postcode
-							}
-						}
-					>
-						<Row>
-							<Col xs={24} sm={24} md={24} lg={24}>
-								<Row gutter={ROW_GUTTER}>
-									<Col xs={24} sm={24} md={12}>
-										<Form.Item
-											label="Name"
-											name="name"
-											rules={[
-												{
-													required: true,
-													message: 'Please input your name!',
-												},
-											]}
-										>
-											<Input />
-										</Form.Item>
-									</Col>
-									<Col xs={24} sm={24} md={12}>
-										<Form.Item
-											label="Username"
-											name="username"
-											rules={[
-												{
-													required: true,
-													message: 'Please input your username!'
-												},
-											]}
-										>
-											<Input />
-										</Form.Item>
-									</Col>
-									<Col xs={24} sm={24} md={12}>
-										<Form.Item
-											label="Email"
-											name="email"
-											rules={[{
-												required: true,
-												type: 'email',
-												message: 'Please enter a valid email!'
-											}]}
-										>
-											<Input />
-										</Form.Item>
-									</Col>
-									<Col xs={24} sm={24} md={12}>
-										<Form.Item
-											label="Date of Birth"
-											name="dateOfBirth"
-										>
-											<DatePicker className="w-100" />
-										</Form.Item>
-									</Col>
-									<Col xs={24} sm={24} md={12}>
-										<Form.Item
-											label="Phone Number"
-											name="phoneNumber"
-										>
-											<Input />
-										</Form.Item>
-									</Col>
-									<Col xs={24} sm={24} md={12}>
-										<Form.Item
-											label="Website"
-											name="website"
-										>
-											<Input />
-										</Form.Item>
-									</Col>
-									<Col xs={24} sm={24} md={24}>
-										<Form.Item
-											label="Address"
-											name="address"
-										>
-											<Input />
-										</Form.Item>
-									</Col>
-									<Col xs={24} sm={24} md={12}>
-										<Form.Item
-											label="City"
-											name="city"
-										>
-											<Input />
-										</Form.Item>
-									</Col>
-									<Col xs={24} sm={24} md={12}>
-										<Form.Item
-											label="Post code"
-											name="postcode"
-										>
-											<Input />
-										</Form.Item>
-									</Col>
-								</Row>
-								<Button type="primary" htmlType="submit">
-									Save Change
-								</Button>
-							</Col>
-						</Row>
-					</Form>
-				</div>
-			</Card>
-		)
-	}
-}
-
-export default EditProfile
+export default EditPorfile;
