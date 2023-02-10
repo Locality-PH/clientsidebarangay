c15a2c2cf735e028aceb24adf9e951558c9733d5
GiannPogi <gcmediavillo@gmail.com>
2023-02-10 18:39:44 +0800

    delete footer

--- src/components/layout-components/Footer.js
+++ src/components/layout-components/Footer.js
@@ -4,26 +4,7 @@ import { APP_NAME } from "configs/AppConfig";
 export default function Footer() {
   return (
     <footer className="footer">
-      <span>
-        Copyright &copy; {`${new Date().getFullYear()}`}{" "}
-        <span className="font-weight-semibold">{`${APP_NAME}`}</span> All rights
-        reserved.
-      </span>
-      <div>
-        <a
-          className="text-gray"
-          href={`http://${window.location.hostname}:${window.location.port}/support/term-condition`}
-        >
-          Term & Conditions
-        </a>
-        <span className="mx-2 text-muted"> | </span>
-        <a
-          className="text-gray"
-          href={`http://${window.location.hostname}:${window.location.port}/support/privacy-policy`}
-        >
-          Privacy & Policy
-        </a>
-      </div>
+      
     </footer>
   );
 }
