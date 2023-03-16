5d6e4b54acead169715d05c8c71b87d70064eeb2
vonypeto <von.aralar@gmail.com>
2023-03-16 16:20:32 +0800

    Debug gian mistake

--- src/views/app-views/account/profile/body/Likes.js
+++ src/views/app-views/account/profile/body/Likes.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Row, Col, Card, message, Divider, Skeleton, Button, message} from "antd";
+import { Row, Col, Card, message, Divider, Skeleton, Button } from "antd";
 import { useAuth } from "contexts/AuthContext";
-import axios from 'axios'
-import moment from 'moment'
+import axios from "axios";
+import moment from "moment";
 import SearchCampaign from "components/shared-components/SearchCampaign";
 
 const Likes = () => {
@@ -15,108 +15,111 @@ const Likes = () => {
   const [loading, setLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
   const [campaignList, setCampaignList] = useState([]);
-  const [pageSetup, setPageSetup] = useState({ page: 1, pageSize: 4, landingPage: "likes", })
+  const [pageSetup, setPageSetup] = useState({
+    page: 1,
+    pageSize: 4,
+    landingPage: "likes",
+  });
 
   //useEffect
   useEffect(() => {
-    getLatestCampaign()
-  }, [])
+    getLatestCampaign();
+  }, []);
 
   useEffect(() => {
-    getLatestCampaign()
-  }, [pageSetup])
+    getLatestCampaign();
+  }, [pageSetup]);
 
   //axios
   const getLatestCampaign = async () => {
-    const { page, pageSize, landingPage } = pageSetup
+    const { page, pageSize, landingPage } = pageSetup;
 
-    setLoading(true)
+    setLoading(true);
 
     try {
-      await axios.get(
-        `/api/campaign/latest?page=${page}&pageSize=${pageSize}&landingPage=${landingPage}`,
-        generateToken()[1],
-        { cancelToken })
-        .then(
-          (res) => {
-            var data = res.data
-            console.log("data", data)
-            setCampaignList([...campaignList, ...data])
-            if (data.length == 0) setHasMore(false)
-          })
+      await axios
+        .get(
+          `/api/campaign/latest?page=${page}&pageSize=${pageSize}&landingPage=${landingPage}`,
+          generateToken()[1],
+          { cancelToken }
+        )
+        .then((res) => {
+          var data = res.data;
+          console.log("data", data);
+          setCampaignList([...campaignList, ...data]);
+          if (data.length == 0) setHasMore(false);
+        });
     } catch (error) {
-      console.log("error", error)
+      console.log("error", error);
       message.error("The action can't be completed, please try again.");
     }
 
-    setLoading(false)
-  }
+    setLoading(false);
+  };
 
   return (
     <Row align="center">
       <Col sm={24} md={18}>
-      <Card title={"Liked Events"}>
-        {campaignList.map((item, i) => {
-          return (
-            <div key={i}>
-              <Divider
-                className="divider-margin"
-                style={{ margin: "0px !important" }}
-              />
-              <Col span={24}>
-                <SearchCampaign
-                  startDate={
-                    new Date(item?.starting_date)
-                  }
-                  profile={
-                    item && item.images && item.images[0]
-                      ? item.images[0].data
-                      : undefined
-                  }
-                  title={item?.title}
-                  address={item?.organization?.address}
-                  name={item?.organization?.organization_name}
-                  id={item?.organization?.organization_id}
-                  campaign_id={item?._id}
-                  link={`/home/posts/63f5a0ea53dca62f010488e6/${item?._id}/single/data`}
+        <Card title={"Liked Events"}>
+          {campaignList.map((item, i) => {
+            return (
+              <div key={i}>
+                <Divider
+                  className="divider-margin"
+                  style={{ margin: "0px !important" }}
                 />
+                <Col span={24}>
+                  <SearchCampaign
+                    startDate={new Date(item?.starting_date)}
+                    profile={
+                      item && item.images && item.images[0]
+                        ? item.images[0].data
+                        : undefined
+                    }
+                    title={item?.title}
+                    address={item?.organization?.address}
+                    name={item?.organization?.organization_name}
+                    id={item?.organization?.organization_id}
+                    campaign_id={item?._id}
+                    link={`/home/posts/63f5a0ea53dca62f010488e6/${item?._id}/single/data`}
+                  />
+                </Col>
+              </div>
+            );
+          })}
+          {loading && (
+            <>
+              <Col span={24}>
+                <Card>
+                  <Skeleton loading={loading} avatar active></Skeleton>
+                </Card>
               </Col>
-            </div>
-          );
-        })}
-        {loading && (
-          <>
-            <Col span={24}>
-              <Card>
-                <Skeleton loading={loading} avatar active></Skeleton>
-              </Card>
-            </Col>
-            <Col span={24}>
-              <Card>
-                <Skeleton loading={loading} avatar active></Skeleton>
-              </Card>
-            </Col>
-            <Col span={24}>
-              <Card>
-                <Skeleton loading={loading} avatar active></Skeleton>
-              </Card>
-            </Col>
-          </>
-        )}
-        <Col justify="center" className="text-center" span={24}>
-          {!loading && hasMore ? 
-            <Button
-              onClick={() => setPageSetup({ ...pageSetup, page: pageSetup.page + 1 })}
-            >
-              Load More
-            </Button>
-
-            :
-
-            "You have reach the end of the list."
-          }
-        </Col>
-      </Card>
+              <Col span={24}>
+                <Card>
+                  <Skeleton loading={loading} avatar active></Skeleton>
+                </Card>
+              </Col>
+              <Col span={24}>
+                <Card>
+                  <Skeleton loading={loading} avatar active></Skeleton>
+                </Card>
+              </Col>
+            </>
+          )}
+          <Col justify="center" className="text-center" span={24}>
+            {!loading && hasMore ? (
+              <Button
+                onClick={() =>
+                  setPageSetup({ ...pageSetup, page: pageSetup.page + 1 })
+                }
+              >
+                Load More
+              </Button>
+            ) : (
+              "You have reach the end of the list."
+            )}
+          </Col>
+        </Card>
       </Col>
     </Row>
   );
